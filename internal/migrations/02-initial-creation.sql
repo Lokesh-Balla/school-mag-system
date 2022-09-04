c95e5b346aa5ea67593c08f0daa700f3a4b80633
satyanarayana kalidindi <satyanarayana@hydlpt103.opsramporg.com>
2022-09-04 22:57:06 +0530

    added references to key

--- internal/migrations/02-initial-creation.sql
+++ internal/migrations/02-initial-creation.sql
@@ -1,12 +1,12 @@
 create TABLE IF NOT EXISTS registration(id int not null  AUTO_INCREMENT,firstname varchar(100) not null,lastname varchar(100) not null,username varchar(30) not null,passwordhash varchar(100) not null,role varchar(30) not null,mailid varchar(30)  not null,phonenumber int not null , bloodgroup varchar(5), PRIMARY KEY(id),UNIQUE(username));
 
-create TABLE IF NOT EXISTS passwordsalts(id int(5) not null  AUTO_INCREMENT,salt varchar(100) not null , PRIMARY KEY(id));
+create TABLE IF NOT EXISTS passwordsalts(saltid int not null  AUTO_INCREMENT,id int not null,salt varchar(100) not null , PRIMARY KEY(saltid),FOREIGN KEY (id) REFERENCES registration(id));
 
-create TABLE IF NOT EXISTS student(id int(5) not null  AUTO_INCREMENT,RegdNumber varchar(30) not null,classid varchar(20) not null , PRIMARY KEY(id));
+create TABLE IF NOT EXISTS student(studentid int not null  AUTO_INCREMENT,studentregistrationid int,classid varchar(20) not null , PRIMARY KEY(studentid),FOREIGN KEY (studentregistrationid) REFERENCES registration(id));
 
-create TABLE IF NOT EXISTS teacher(id int(5) not null AUTO_INCREMENT, empid varchar(30) not null,designation varchar(30) not null , PRIMARY KEY(id));
+create TABLE IF NOT EXISTS teacher(teacherid int not null AUTO_INCREMENT, teacherregistrationid int not null,designation varchar(30) not null , PRIMARY KEY(teacherid),FOREIGN KEY (teacherregistrationid) REFERENCES registration(id));
 
-create TABLE IF NOT EXISTS parent(id int(5) not null AUTO_INCREMENT , parentid varchar(30) not null, relation varchar(30) not null , PRIMARY KEY(id));
+create TABLE IF NOT EXISTS parent(parentid int not null AUTO_INCREMENT , parentregistrationid int not null, relation varchar(30) not null , PRIMARY KEY(parentid),FOREIGN KEY (parentregistrationid) REFERENCES registration(id));
 
 create TABLE IF NOT EXISTS class(classid int not null AUTO_INCREMENT , classname varchar(30) not null, PRIMARY KEY(classid));
 
